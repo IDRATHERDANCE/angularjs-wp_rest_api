c62fbcd2fe9f9e0bd7ed11df1a72a8c48c1cc081
IDRATHER.DANCE <yes@idrather.dance>
2016-04-19 19:14:02 +0200

    compass added to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,27 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     concat = require('gulp-concat'),
-    browserify = require('gulp-browserify')
+    browserify = require('gulp-browserify'),
+    compass = require('gulp-compass')
     
 
 var sassSources = ['sass/styles.scss'];
 var jsSources = ['components/angular/*.js', 'components/scripts/*.js'];
+var sassSources = ['components/sass/styles.scss'];
 
-//    gulp.task('sass', function(){
-//        gulp.src(sassSources)
-//        
-//    });
+    gulp.task('sass', function(){
+        gulp.src(sassSources)
+            .pipe(compass({
+                sass: 'components/sass',
+                image: 'builds/development/images',
+                style: 'expanded',
+                require: 'breakpoint'
+            }))
+
+            .on('error', gutil.log)
+            .pipe(gulp.dest('builds/development/css'))
+        
+    });
 
 
     gulp.task('js', function(){
